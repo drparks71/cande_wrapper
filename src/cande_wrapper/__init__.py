efb6e3fb2af1334ddecac0039046f9d137e9eadf
dane <dane@daneparks.com>
2026-04-06 22:04:05 -0400

    Version bump to 0.1.0, added Fortran CANDE source files, and updated project dependencies

--- src/cande_wrapper/__init__.py
+++ src/cande_wrapper/__init__.py
@@ -1,5 +1,5 @@
 """cande-wrapper: Python wrapper for the CANDE FEA culvert analysis engine."""
-print('Hello from cande-wrapper!')
-# from cande_wrapper.engine import CandeEngine, CandeResult
-#
-# __all__ = ["CandeEngine", "CandeResult"]
+
+from cande_wrapper.engine import CandeEngine, CandeResult
+
+__all__ = ["CandeEngine", "CandeResult"]
